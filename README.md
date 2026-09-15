6a86131f078c6cbd599dcbfe16875ffb6820c147
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-15 13:02:08 -0400

    I change the colour of my subheading to made more darker and different from the heading
    Bookmarks contain my journal and future prototypes. So I want to made a difference with the title (Bookmarks) and the subsection.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Hello, I'm Mariam-Choukri Mahdi, an student in Computation Art program.
 I wish as the website growth, I growth too. They could be mistake done or perfection accomplish. 
 No matter what, it’s my dimension and you enter it.  </p>
 <h2 style="color:rgb(101, 78, 200);">Bookmarks of all favorites</h2>
-<h3 style="color:rgb(101, 78, 200);">My journal</h3>
+<h3 style="color:rgb(81, 63, 159);">My journal</h3>
 [My reflective journal](journal.md)<br>
-<h3 style="color:rgb(101, 78, 200);">Future Prototypes</h3> 
+<h3 style="color:rgb(81, 63, 159);">Future Prototypes</h3> 
 Nothings currently right now :sleepy:<br>
---
+++ topics/instructions/drawing-practice/js/script.js
@@ -0,0 +1,46 @@
+/**
+ * Title of Project
+ * Mariam-Choukri Mahdi
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED
+ *  Uses:
+ * p5.js
+ * https://p5js.org
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    //Create a nice canvas to work with it
+    createCanvas(640, 640);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    //A grey background
+    background(150, 150, 150);
+    //This how you can draw a circle
+    ellipse(320, 320, 480, 480);
+    //This how you can draw a rectangle
+    rect(200, 60, 200, 300);
+    // This how you can have outline and inline with different of a shape 
+    fill(255,0,0)
+    stroke(255, 255, 255)
+    ellipse(320, 320, 480, 480);
+    //This how we can reset our setting
+    pop();
+/** 
+ * Draws a fluffy white cloud
+*/
+function drawCloud(){
+    // A cloud 
+
+}
+}
