a9ef9000d8c6ce0cfac40e38260560e275b1d8af
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-15 11:36:29 -0400

    I have created or modified the comments on my script.js.

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,25 +1,30 @@
 /**
- * Function Calls
+ * The Window
  * Mariam-Choukri Mahdi 
  * 
- *I'm working on the function calls example give by the professor. 
+ * It's the boredom of the average human.
+ * Who zone out in attempt to entertain himself. 
+ * 
+ * The program is non-interactive to represent the non existent of dreams. 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the base of our dreams
 */
 function setup() {
-    // Once at the beginning of the program
+    // Create the canvas with wider resolution than the norm 
     createCanvas(540, 780);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draw the person's window and the piece imagination in the window's refelction
 */
 function draw() {
-    // Every frame
+    // The purple room
     background(200, 155, 255);
+    // The window
+    rect(150, 200, 240, 320); 
 }
