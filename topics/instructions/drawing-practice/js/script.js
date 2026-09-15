38a2aa1aeb1d0ced7462772ac0efb36d6bc0b61d
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-15 16:30:06 -0400

    We finish the mountains and added our first creature.
    The scenic background is done and we add a bird.

--- topics/instructions/drawing-practice/js/script.js
+++ topics/instructions/drawing-practice/js/script.js
@@ -29,6 +29,7 @@ function draw() {
     //A yellow circle
     fill(255,255,36)
     stroke(255,152,20)
+    strokeWeight(10)
     ellipse(320, 320, 250, 250);
 
     //A yellow rectangle and a blue rectangle
@@ -40,6 +41,21 @@ function draw() {
     stroke(68,154,178)
      rect(-10,500,660,150)
 
+    // Adding scenic background of triangle 
+    fill(112,97,66)
+    stroke(112,97,66)
+    triangle(-10,340,80,40,200,340)
+    triangle(450,340,550,40,650,340)
 
-    
+    fill(143,125,99)
+    stroke(143,125,99)
+    triangle(-10,340,70,180,150,340)
+    triangle(490,340,570,180,650,340)
+
+    //Draw a bird
+    stroke(0,0,0)
+    strokeWeight(3)
+    noFill()
+    arc(100,100,40,30,PI,0)
+    arc(140,100,40,30,PI,0)
 }
