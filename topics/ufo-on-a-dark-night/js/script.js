b74287e5febe729ab30d2f303a2eae7089557437
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-20 18:46:22 -0400

    Used variable to make the Ufo go back to his planet

--- topics/ufo-on-a-dark-night/js/script.js
+++ topics/ufo-on-a-dark-night/js/script.js
@@ -1,6 +1,6 @@
 /**
  * UFO on a Dark Night
- * Pippin Barr
+ * Mariam-Choukri Mahdi
  * 
  * A UFO. On a dark night. It just sits there?
  */
@@ -33,8 +33,22 @@ function setup() {
  * Displays a UFO
 */
 function draw() {
+    // Dawn
+    skyShade = skyShade +1;
     // Display the sky
     background(skyShade);
+
+    //Move the UFO 
+    ufo.x = ufo.x + 1.5;
+    ufo.y = ufo.y - 5;
+
+    // Change its fill
+    ufo.fill = ufo.fill * 0.500;
+
+    // Change its shape
+    ufo.width = ufo.width / 1.008;
+    ufo.height = ufo.height / 1.008;
+
 
     // Draw the UFO based on its properties
     push();
