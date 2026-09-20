9a534f32935f27cb07954fd6b7757e8d8ebdd8f2
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-20 18:27:57 -0400

    Changed my name to the project and downgraded my self esteem

---
+++ topics/self-esteem/js/script.js
@@ -0,0 +1,64 @@
+/**
+ * Self-Esteem
+ * Mariam-Choukri Mahdi
+ * 
+ * A portrait of Pippin's self-esteem on a sunny day.
+ */
+
+"use strict";
+
+// Colour of the sky
+let skyRed = 150;
+let skyGreen = 180;
+let skyBlue = 250;
+
+// The sun
+
+
+let sun = {
+    red: 100,
+    green: 100,
+    blue: 0,
+    x: 500,
+    y: 70,
+    size: 100,
+};
+
+// My self-esteem
+
+// Greyscale shade
+let selfEsteemShade = 0;
+// Position and size
+let selfEsteemX = 320;
+let selfEsteemY = 320;
+let selfEsteemSize = 20;
+
+/**
+ * Create the canvas
+ */
+function setup() {
+    // Create the canvas
+    createCanvas(640, 320);
+}
+
+/**
+ * Displays the sky, sun, and self-esteem
+ */
+function draw() {
+    // A nice blue sky
+    background(skyRed, skyGreen, skyBlue);
+
+    // The sun
+    push();
+    fill(sun.red, sun.green, sun.blue);
+    noStroke();
+    ellipse(sun.x, sun.y, sun.size);
+    pop();
+
+    // My self esteem
+    push();
+    fill(selfEsteemShade);
+    noStroke();
+    ellipse(selfEsteemX, selfEsteemY, selfEsteemSize);
+    pop();
+}
