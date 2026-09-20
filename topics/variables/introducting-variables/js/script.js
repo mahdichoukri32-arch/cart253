e3aeb9e55fa508b5da21d5869c09c4e37ed227f6
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-20 17:43:23 -0400

    Started the creating variables project

--- topics/variables/introducting-variables/js/script.js
+++ topics/variables/introducting-variables/js/script.js
@@ -1,24 +1,38 @@
 /**
- * Title of Project
- * Author Name
+ * Introducing variables
+ * Mariam-Choukri Mahdi
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Learning what a variable is and does
+ *
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create a canvas
 */
 function setup() {
-
+    createCanvas(640, 480);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    background(0);
+    
+    // Draw a circle in the centre of the canvas
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(320, 320, 100, 100);
+    pop();
+}
 
+function draw() {
+    background(0);
+    
+    // Draw a circle in the centre of the canvas
+    push();
+    noStroke();
+    fill(mouseX, mouseY, 0);
+    ellipse(width/2,height/2, mouseX, mouseY);
+    pop();
 }
