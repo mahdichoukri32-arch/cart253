2be75d6705ef5414829eedeb579d162c96a9e997
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-21 15:33:17 -0400

    Started the confusion project

---
+++ topics/confusion-project/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Confusion
+ * Mariam-Choukri Mahdi
+ * 
+ * You seem confused and it is visble throught your eyes. 
+ */
+
+"use strict";
+
+/**
+ * 
+*/
+function setup() {
+ 
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+ 
+
+
+}
