b33a2e82f5764e608601fdd6c2f870dc60fba2e8
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-21 17:09:13 -0400

    Added the face feature and made moving the iris/pupil
    With the movement of the iris/pupil in one way and other it create a sense of confusion in the face

--- topics/confusion-project/js/script.js
+++ topics/confusion-project/js/script.js
@@ -7,6 +7,32 @@
 
 "use strict";
 
+//  The iris
+let irisLeft= {
+    x: 250,
+    y: 250,
+    size: 20,
+    minX: 275,
+    maxX: 275,
+    minY: 225,
+    maxY: 275,
+    fill: 0
+};
+
+let irisRight= {
+    x: 450, 
+    y: 250,
+    size: 20,
+    minX: 425,
+    maxX: 475,
+    minY: 225,
+    maxY: 275,
+    fill: 0
+}
+
+
+
+
 /**
  * Create a canvas
 */
@@ -14,6 +40,7 @@ function setup() {
         createCanvas(640, 480);
 
 }
+
 
 
 /**
@@ -44,12 +71,37 @@ function draw() {
     ellipse(450, 250, 100);
     pop();
 
+    //Uptade the iris
+    // Constrain the iris to be within its defined minimum and maximum
+    irisLeft.x = constrain(irisLeft.x, irisLeft.minX, irisLeft.maxX);
+    irisLeft.y = constrain(irisLeft.y, irisLeft.minY, irisLeft.maxY);
+    irisRight.x = constrain(irisRight.x, irisRight.minX, irisRight.maxX);
+    irisRight.y = constrain(irisRight.y, irisRight.minY, irisRight.maxY);
+    //Move the iris
+    irisLeft.x = irisLeft.x + 1;
+    irisLeft.y = irisLeft.y - 0.5;
+    irisRight.x = irisRight.x - 1;
+    irisRight.y = irisRight.y + 0.5;
+
+
     //Draw the iris 
+    push();
+    noStroke();
+    fill(irisLeft.fill);
+    ellipse(irisLeft.x, irisLeft.y, irisLeft.size);
+    pop();
+
+    push();
+    noStroke();
+    fill(irisRight.fill);
+    ellipse(irisRight.x, irisRight.y, irisRight.size);
+    pop();
 
     //Draw the mouth 
     push();
     fill(210, 43, 43);
     stroke(0, 0, 0);
     strokeWeight(2);
-    ellipse(350, 350, 50)
+    ellipse(350, 350, 50);
+    pop();
 }
