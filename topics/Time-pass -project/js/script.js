7123bdfebbdd3a7c3d2ade8e1633f39f7556e426
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-21 17:22:47 -0400

    Started the time passing project

---
+++ topics/Time-pass -project/js/script.js
@@ -0,0 +1,25 @@
+/**
+ * Did you see the time ?
+ * Mariam-Choukri Mahdi
+ * 
+ * Time have been passing faster than you thinks. 
+ */
+
+"use strict";
+
+/**
+ * Create the canvas
+*/
+function setup() {
+
+
+}
+
+
+/**
+ * Draw a background where the day pass it when we move the mouse.
+ * The mouse change the colour of the sky,sun/moon and field.  
+*/
+function draw() {
+
+}
