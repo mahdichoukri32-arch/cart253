f63e40f4b263d3a02007b06c81abbe4ae60c0a02
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-21 15:52:35 -0400

    Draw the face

--- topics/confusion-project/js/script.js
+++ topics/confusion-project/js/script.js
@@ -8,19 +8,48 @@
 "use strict";
 
 /**
- * 
+ * Create a canvas
 */
 function setup() {
- 
+        createCanvas(640, 480);
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draw a face where the iris is moving inside the eyes.
 */
 function draw() {
- 
+    background(100);
+    //Draw the face 
+    push();
+    fill(160,82,45);
+    stroke(139, 69, 19);
+    strokeWeight(5);
+    ellipse(320, 240, 400);
+    pop();
 
+    //Draw the eyes
+    push();
+    fill(255, 255, 255);
+    stroke(0, 0, 0);
+    strokeWeight(2);
+    ellipse(250, 250, 100);
+    pop();
 
+      push();
+    fill(255, 255, 255);
+    stroke(0, 0, 0);
+    strokeWeight(2);
+    ellipse(450, 250, 100);
+    pop();
+
+    //Draw the iris 
+
+    //Draw the mouth 
+    push();
+    fill(210, 43, 43);
+    stroke(0, 0, 0);
+    strokeWeight(2);
+    ellipse(350, 350, 50)
 }
