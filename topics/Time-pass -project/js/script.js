3f969cbfb5f13d26208c47bb3909e9b57af98aca
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-22 10:22:15 -0400

    Used the mouseX function to create an transition between day to night
    The transition impact the colour of each element of the background (the sky, the sun, the beach and the sea)

--- topics/Time-pass -project/js/script.js
+++ topics/Time-pass -project/js/script.js
@@ -7,6 +7,31 @@
 
 "use strict";
 
+let sky = {
+    red: 0,
+    green: 191,
+    blue: 255
+}
+
+let sun = {
+    red: 255,
+    green: 215,
+    blue: 0
+}
+
+let beach = {
+    red: 250,
+    green: 250,
+    blue: 210
+}
+
+let sea = {
+    red: 175, 
+    green: 238,
+    blue: 238
+}
+
+
 /**
  * Create the canvas
 */
@@ -22,30 +47,51 @@ function setup() {
  * The mouse change the colour of the sky,sun/moon and field.  
 */
 function draw() {
-    background(0, 191, 255);
+
+    // Change the colour of the sky
+    sky.green = 191 - mouseX / 3;
+    sky.blue = 255 - mouseX / 3;
+
+    sky.green = constrain(191 - mouseX / 3, 20, 191);
+    sky.blue = constrain(255 - mouseX / 3, 60, 255);
+
+    // Do the same for sun/moon
+    sun.red = 255 - mouseX / 25;
+    sun.blue = mouseX / 4;
+    sun.green = 215 + mouseX / 40;
+    
+    // Change the colour of the beach
+    beach.red =  250 - mouseX / 5;
+    beach.blue = 210 - mouseX / 5;
+    beach.green = 250 - mouseX / 5; 
+
+    // Do the same for the sea
+    sea.red = 175 - mouseX / 5;
+    sea.green = 238 - mouseX / 5;
+    sea.blue = 238 - mouseX / 5;
+
+
+
+    background(sky.red, sky.green, sky.blue);
+
     //Draw a sun or moon
     push();
-    fill(255, 215, 0);
+    fill(sun.red, sun.green, sun.blue);
     noStroke();
     ellipse(500, 150,130);
     pop();
 
     //Draw the beach 
     push();
-    fill(250, 250, 210);
+    fill(beach.red, beach.green, beach.blue);
     noStroke();
     rect(0, 320, 680, 100);
     pop();
 
     //Draw the sea
     push();
-    fill(175, 238, 238);
+    fill(sea.red, sea.green, sea.blue);
     noStroke();
     rect(-10, 380, 670, 165);
     pop();
-
-
-    
-
-
 }
