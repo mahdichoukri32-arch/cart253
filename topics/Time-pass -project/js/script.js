313643af4ef24152dcda2a465dbf7e1f2a6cd776
mahdichoukri32-arch <mahdichoukri32@gmail.com>
2026-09-21 22:57:06 -0400

    Create the canvas and draw the element of background
    This drawings represent a deep blue sky with bright sun follow by white-sand beach and turquoise water.

--- topics/Time-pass -project/js/script.js
+++ topics/Time-pass -project/js/script.js
@@ -11,6 +11,7 @@
  * Create the canvas
 */
 function setup() {
+    createCanvas(640, 480);
 
 
 }
@@ -21,5 +22,30 @@ function setup() {
  * The mouse change the colour of the sky,sun/moon and field.  
 */
 function draw() {
+    background(0, 191, 255);
+    //Draw a sun or moon
+    push();
+    fill(255, 215, 0);
+    noStroke();
+    ellipse(500, 150,130);
+    pop();
+
+    //Draw the beach 
+    push();
+    fill(250, 250, 210);
+    noStroke();
+    rect(0, 320, 680, 100);
+    pop();
+
+    //Draw the sea
+    push();
+    fill(175, 238, 238);
+    noStroke();
+    rect(-10, 380, 670, 165);
+    pop();
+
+
+    
+
 
 }
